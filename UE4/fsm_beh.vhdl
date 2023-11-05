3cd4e1cd13416abd7ec43d9f53a2ff2fbad259cc
root <root@DESKTOP-ALPBO6B>
2023-11-05 14:51:31 +0100

    UE4 is done

--- UE4/fsm_beh.vhdl
+++ UE4/fsm_beh.vhdl
@@ -4,6 +4,67 @@ use work.fsm_pkg.all;
 
 architecture behavior of fsm is
 
+    --signal current_state:fsm_state := START;
+
 begin
+
+    clkpr: process (CLK)
+     
+        variable output_value:std_logic_vector(1 downto 0) := "00";
+        variable current_state:fsm_state := START;
+    begin
+       
+        if rising_edge(CLK) then
+
+            if RST = '1' then
+                current_state := START;
+                output_value := "00";
+            else
+                if INPUT = "00" then
+                    if current_state = START then
+                        current_state := S2;
+                        output_value := "01";
+                    elsif current_state = S2 then
+                        current_state := S2;
+                        output_value := "10";
+                    elsif current_state = S0 then
+                        current_state := S2;
+                        output_value := "10";
+                    else 
+                        output_value := "00";
+                    end if;
+                elsif INPUT = "11" then
+                    if current_state = S2 then
+                        current_state := S0;
+                        output_value := "01";
+                    elsif current_state = S0 then
+                        current_state := S1;
+                        output_value := "01";
+                    elsif current_state = S1 then
+                        current_state := S0;
+                        output_value := "10";
+                    else 
+                        output_value := "00";
+                    end if;
+                elsif INPUT = "01" then
+                    if current_state = S1 then
+                        current_state := S1;
+                        output_value := "00";
+                    elsif current_state = S0 then
+                        current_state := S0;
+                        output_value := "10";
+                    else
+                        output_value := "00";
+                    end if;
+                else
+                    output_value := "00";
+                end if;
+            end if;
+        end if;
+
+        OUTPUT <= output_value;
+        STATE <= current_state;
+
+    end process clkpr;
 
 end behavior;
