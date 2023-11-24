6edc699aac8f3796dd7889b145cc63e65c324b00
root <root@DESKTOP-ALPBO6B>
2023-11-24 23:17:08 +0100

    UE5 is done

--- UE5/RAM_beh.vhdl
+++ UE5/RAM_beh.vhdl
@@ -4,7 +4,7 @@ use IEEE.Numeric_Std.all;
 
 architecture Behavioral of RAM is
 
-    subtype word_t is std_logic_vector(0 downto 13);
+    subtype word_t is std_logic_vector(13 downto 0);
     type memory_t is array(0 to 255) of word_t;
     signal ram_data : memory_t := (others => (others => '0'));
 
@@ -25,23 +25,23 @@ begin
                     output2 <= (others => 'Z');
                 
                     if(en_write = '1') then
-                        ram_data(to_integer(unsigned(addr1))) <= input(0 downto 13);
-                        ram_data(to_integer(unsigned(addr1))+1) <= input(14 downto 27);
+                        ram_data(to_integer(unsigned(addr1))) <= input(13 downto 0);
+                        ram_data(to_integer(unsigned(addr1))+1) <= input(27 downto 14);
                     end if;
 
                 elsif(en_read1 = '0' and en_read2 = '1') then
                     output1 <= (others => 'Z');
                     if(en_write = '0') then                                                    --0 1 0
-                        output2(0 downto 13) <= ram_data(to_integer(unsigned(addr2)));
-                        output2(14 downto 27) <= ram_data(to_integer(unsigned(addr2))+1);
+                        output2(13 downto 0) <= ram_data(to_integer(unsigned(addr2)));
+                        output2(27 downto 14) <= ram_data(to_integer(unsigned(addr2))+1);
                     else                                                                       --0 1 1
                         if((addr1 /= addr2) and 
                         (to_integer(unsigned((addr2))) /= to_integer(unsigned((addr1))+1)) and 
                         (to_integer(unsigned((addr1))) /= to_integer(unsigned((addr2))+1))) then
-                            output2(0 downto 13) <= ram_data(to_integer(unsigned(addr2)));
-                            output2(14 downto 27) <= ram_data(to_integer(unsigned(addr2))+1);
-                            ram_data(to_integer(unsigned(addr1))) <= input(0 downto 13);
-                            ram_data(to_integer(unsigned(addr1))+1) <= input(14 downto 27);
+                            output2(13 downto 0) <= ram_data(to_integer(unsigned(addr2)));
+                            output2(27 downto 14) <= ram_data(to_integer(unsigned(addr2))+1);
+                            ram_data(to_integer(unsigned(addr1))) <= input(13 downto 0);
+                            ram_data(to_integer(unsigned(addr1))+1) <= input(27 downto 14);
                         else
                             output2 <= (others => 'Z');
                         end if;   
@@ -50,18 +50,18 @@ begin
                 elsif(en_read1 = '1' and en_read2 = '0') then
                     output2 <= (others => 'Z');
                     if(en_write = '0') then                                                    --1 0 0
-                        output1(0 downto 13) <= ram_data(to_integer(unsigned(addr1)));
-                        output1(14 downto 27) <= ram_data(to_integer(unsigned(addr1))+1);
+                        output1(13 downto 0) <= ram_data(to_integer(unsigned(addr1)));
+                        output1(27 downto 14) <= ram_data(to_integer(unsigned(addr1))+1);
                     else                                                                       --1 0 1
                         output1 <= (others => 'Z');
                     end if;
 
-                elsif(en_read1 = '1' and en_read2 = '0') then 
-                    if(en_write = '0' and (addr1 /= addr2)) then                                                    -- 1 1 0
-                        output1(0 downto 13) <= ram_data(to_integer(unsigned(addr1)));
-                        output1(14 downto 27) <= ram_data(to_integer(unsigned(addr1))+1);
-                        output2(0 downto 13) <= ram_data(to_integer(unsigned(addr2)));
-                        output2(14 downto 27) <= ram_data(to_integer(unsigned(addr2))+1);
+                elsif(en_read1 = '1' and en_read2 = '1') then 
+                    if(en_write = '0') then                                                    -- 1 1 0
+                        output1(13 downto 0) <= ram_data(to_integer(unsigned(addr1)));
+                        output1(27 downto 14) <= ram_data(to_integer(unsigned(addr1))+1);
+                        output2(13 downto 0) <= ram_data(to_integer(unsigned(addr2)));
+                        output2(27 downto 14) <= ram_data(to_integer(unsigned(addr2))+1);
                     else                                                                       -- 1 1 1
                         output1 <= (others => 'Z');
                         output2 <= (others => 'Z');
